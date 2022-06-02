157260fb674fa86dee431c1edafa8879b04cde4c
akshayyt <106232049+akshayyt@users.noreply.github.com>
2022-06-02 10:31:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/RoyalKrrishna/MdiskWalaBot.git /MdiskWalaBot
+git clone https://github.com/akshayyt/MdiskWalaBot.git /MdiskWalaBot
 cd /MdiskWalaBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
